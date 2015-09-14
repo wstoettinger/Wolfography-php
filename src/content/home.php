4df254b5568eb80f173d2fd250fce068d17fbb64
w.stoettinger <wolfgang.stoettinger@gmail.com>
2015-09-14 08:39:55 +0200

    x

--- src/content/home.php
+++ src/content/home.php
@@ -15,12 +15,12 @@ ob_start();
       </div>      
     </div>
   </div>
-  <?php echo $page->build_card('/fotos/architektur', 'img/cards/DSC_5959.jpg', 'Architektur', "Some quick example text to build on the card title and make up the bulk of the card's content."); ?>
-  <?php echo $page->build_card('/fotos/produkte', 'img/cards/DSC_5283.jpg', 'Produkte', "Some quick example text to build on the card title and make up the bulk of the card's content."); ?>
-  <?php echo $page->build_card('/fotos/portraits', 'img/cards/DSC_7379.jpg', 'Portraits', "Some quick example text to build on the card title and make up the bulk of the card's content."); ?>
-  <?php echo $page->build_card('/fotos/feste', 'img/cards/DSC_5665.jpg', 'Feste', "Some quick example text to build on the card title and make up the bulk of the card's content."); ?>
-  <?php echo $page->build_card('/fotos/besondere_anlaesse', 'img/cards/DSC_6672.jpg', 'Besondere Anlässe', "Some quick example text to build on the card title and make up the bulk of the card's content."); ?>
-  <?php echo $page->build_card('/fotos/sport', 'img/cards/DSC_7516.jpg', 'Sport', "Some quick example text to build on the card title and make up the bulk of the card's content."); ?>
+  <?php echo $page->build_card('/fotos/architektur', 'img/cards/DSC_5959.jpg', 'Hotel & Architektur', "Ob enge Räume oder weite Fassaden, bei der Architekturfotografie fasziniert mich das Spiel mit Licht, Linien und Proportionen."); ?>
+  <?php echo $page->build_card('/fotos/produkte', 'img/cards/DSC_5283.jpg', 'Produkte & Stills', "Komposition, Perspektive und gezielte Reflektionen sind meine Werkzeuge für tolle Produktfotos, nicht Photoshop."); ?>
+  <?php echo $page->build_card('/fotos/portraits', 'img/cards/DSC_7379.jpg', 'Portraits & Business', "Eine angenehme Atmosphäre ist für mich das wichtigste, um die schönsten Seiten eines jeden Menschen einzufangen."); ?>
+  <?php echo $page->build_card('/fotos/reportagen', 'img/cards/DSC_5665.jpg', 'Reportagen & Events', "Im richtigen Moment abzudrücken, und das am besten unbemerkt, so lassen sich Veranstaltungen am natürlichsten festhalten."); ?>
+  <?php echo $page->build_card('/fotos/besondere_anlaesse', 'img/cards/DSC_6672.jpg', 'Besondere Anlässe',  "Um besondere Ereignisse in schöner Erinnerung zu behalten, bedarf es besonderer Aufmerksamkeit und Geduld."); ?>
+  <?php echo $page->build_card('/fotos/sport', 'img/cards/DSC_7516.jpg', 'Sport',                           "Die richtige Position, Perspektive und das perfekte Timing sind maßgeblich für eindrucksvolle Sport Fotos."); ?>
 </div>
 <?php
 $page->content = ob_get_clean();
